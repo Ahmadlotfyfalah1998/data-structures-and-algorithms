baf09a002da5b29ac1895e09f1f3f9e35d5f90ad
Ahmadlotfyfalah1998 <ahmad.lotfy.falah@gmail.com>
2023-05-06 17:00:21 +0000

    fix code-challenge1

--- array-reverse/array-reverse.py
+++ array-reverse/array-reverse.py
@@ -1,16 +1,16 @@
 
-test=[10,20,30,40,50,60]
-
-
-def reverse_array (arr):
-   
-   l=[]
-   for i in arr:
-    l.insert(0,i)
-   print(l)
-   
-   
-    
-    
-    
-reverse_array(test)
+test=[1,2,3,4,5,6]
+test2=[89, 2354, 3546, 23, 10, -923, 823, -12]
+def reverse_array (arr):  
+   newArr=[]
+   i= len(arr)
+   while i >0:
+    newArr.append(arr[i-1])
+    i=i-1 
+   return newArr
+print(reverse_array(test))
+print(reverse_array(test2))
+"""
+    [6, 5, 4, 3, 2, 1]
+    [-12, 823, -923, 10, 23, 3546, 2354, 89]
+"""
