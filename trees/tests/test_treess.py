f031b4cb217e68d1ee901559721c65c683d2385f
Ahmadlotfyfalah1998 <ahmad.lotfy.falah@gmail.com>
2023-06-11 03:31:56 +0000

    done

---
+++ trees/tests/test_treess.py
@@ -0,0 +1,99 @@
+import  pytest
+from trees.treess import Tree, Node,Tnode,Queue,Binary_search
+
+def test_pre_order():
+   treee = Tree()
+   treee.root=Tnode('root')
+   treee.root.left=Tnode('left')
+   treee.root.right=Tnode('right')
+   treee.root.left.left=Tnode('left,left')
+   treee.root.left.right=Tnode('left,right')
+   treee.root.right.left=Tnode('right,left')
+   treee.root.right.right=Tnode('right,right')
+   actual=treee.pre_order()
+   expected=['root', 'left', 'left,left', 'left,right', 'right', 'right,left', 'right,right']
+   assert actual==expected
+   
+def test_in_order():
+   treee = Tree()
+   treee.root=Tnode('root')
+   treee.root.left=Tnode('left')
+   treee.root.right=Tnode('right')
+   treee.root.left.left=Tnode('left,left')
+   treee.root.left.right=Tnode('left,right')
+   treee.root.right.left=Tnode('right,left')
+   treee.root.right.right=Tnode('right,right')
+   actual=treee.in_order()
+   expected=['left,left', 'left', 'left,right', 'root', 'right,left', 'right', 'right,right']
+   assert actual==expected   
+   
+def test_post_order():
+   treee = Tree()
+   treee.root=Tnode('root')
+   treee.root.left=Tnode('left')
+   treee.root.right=Tnode('right')
+   treee.root.left.left=Tnode('left,left')
+   treee.root.left.right=Tnode('left,right')
+   treee.root.right.left=Tnode('right,left')
+   treee.root.right.right=Tnode('right,right')
+   actual=treee.post_order()
+   expected=['left,left', 'left,right', 'left', 'right,left', 'right,right', 'right', 'root']
+   assert actual==expected    
+   
+   
+def test_empty_tree():
+   treee = Tree()
+   
+   actual=treee.pre_order()
+   expected=None
+   assert actual==expected 
+   
+   
+def test_one_tnode_tree():
+      treee = Tree()
+      treee.root=Tnode('root')
+      actual=treee.pre_order()
+      expected=['root']
+      assert actual==expected 
+      
+      
+def test_binary_tree():
+   binary=Binary_search()
+   binary.add(4)
+   binary.add(2)
+   binary.add(5)
+   binary.add(1)
+   binary.add(3)
+   binary.add(7)
+   binary.add(8)
+   actual=binary.in_order()
+   expected=[1, 2, 3, 4, 5, 7, 8]
+   assert actual==expected
+   
+   
+   
+def test_contain_true():
+   binary=Binary_search()
+   binary.add(4)
+   binary.add(2)
+   binary.add(5)
+   binary.add(1)
+   binary.add(3)
+   binary.add(7)
+   binary.add(8)
+   actual=binary.contains(7)
+   expected=True
+   assert actual==expected
+   
+def test_contain_false():
+   binary=Binary_search()
+   binary.add(4)
+   binary.add(2)
+   binary.add(5)
+   binary.add(1)
+   binary.add(3)
+   binary.add(7)
+   binary.add(8)
+   actual=binary.contains(30)
+   expected=False
+   assert actual==expected
---
+++ trees/trees.md
@@ -0,0 +1,22 @@
+# Challenge Title
+code challenge 15: trees<br>
+
+## Whiteboard Process
+
+
+## Approach & Efficiency
+1. i create tnode class <br>
+2. i create a tree class with some method to traverse the tree <br>
+like in order,pre order, post order<br>
+3. and i create a binary tree class that will set the smallest values on the left always and biggest on the right <br>
+big O :<br>
+for tree class O(n) <br>
+for the binary search class O(n) <br>
+space :<br>
+to tree class O(n) <br>
+to the binary search class O(1)<br>
+
+
+## Solution
+python treess.py<br>
+test----->pytest<br>
