a956ac31412cfb31c9ee866fb359acdd7cb87043
Ahmadlotfyfalah1998 <ahmad.lotfy.falah@gmail.com>
2023-07-15 20:08:34 +0000

    done

---
+++ insert_sort/insert_sort.py
@@ -0,0 +1,15 @@
+def insert(sorted, value):
+    i = 0
+    while i < len(sorted) and value > sorted[i]:
+        i += 1
+    sorted.insert(i, value)
+
+
+def insertion_sort(input):
+    sorted = [input[0]]
+    for i in range(1, len(input)):
+        insert(sorted, input[i])
+    return sorted
+input_array = [5, 2, 9,0,22,99 ,44,2, 1, 3]
+sorted_array = insertion_sort(input_array)
+print(sorted_array) 
